8cf75a933aac9c2fe65e5ba7386e3416e69d6566
Guilherme Garros <62261737+GuiGarros@users.noreply.github.com>
2022-03-08 20:50:14 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import Rotas from './src/rotas'
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import TelaPadrao from './src/componentes/TelaPadrao'
+
+export default function App() {
+
+
+  return <TelaPadrao>
+     <Rotas/> 
+  </TelaPadrao> 
+ 
+}
+
